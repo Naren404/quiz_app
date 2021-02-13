d28fa2d9d764db0c12d70fda7c1f59727cebe0dc
Naren404 <narendrasunar47@gmail.com>
2021-02-13 13:47:51 +0545

    test setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
